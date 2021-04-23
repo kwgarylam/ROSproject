771582dec77fb905cd3e9f45a2128f6b1544bbd2
kwgarylam <kwgarylam@gmail.com>
2021-04-23 23:59:35 +0800

    Update carPower.py

--- reference/carPower.py
+++ reference/carPower.py
@@ -14,19 +14,24 @@ day = 1
 timeTick = 0
 
 # Scaling Factor
-A = 32
+# A = 32
 
 # List to store the car objects
 cars = []
 writeData = []
 data = []
+data2 = []
 mytime = []
 
+inputList = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2]
+#inputList = [3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2]
+#print(len(inputList))
 
 class Car:
-    def __init__(self, name, power, carCreate, carDuration, charging):
+    def __init__(self, name, power, valueA, carCreate, carDuration, charging):
         self.name = name
         self.power = power
+        self.valueA = valueA
         self.carCreate = carCreate
         self.carDuration = carDuration
         self.charging = charging
@@ -62,6 +67,7 @@ with open('output.csv', 'a', newline='') as csvfile:
     writer.writerow(['time', 'cars'])
 
     i = 0
+    j = 0
     while day <= 2:
         # 8am-8pm
         if 0 <= time <= 480 or 1200 <= time < 1440:
@@ -71,12 +77,10 @@ with open('output.csv', 'a', newline='') as csvfile:
             print("Day ", day, "Doing")
             print("Time now: ", str(hh) + ':' + str(mm))
 
-            # Create new objects. There are two cars enter for every cycle
-            cars.append(Car('car' + str(i), 0, timeTick, 0, "Charge"))
-            i = i + 1
-            cars.append(Car('car' + str(i), 0, timeTick, 0, "Charge"))
-            i = i + 1
-
+            for x in range (inputList[j]):
+                # Create new objects. There are two cars enter for every cycle
+                cars.append(Car('car' + str(i), 0, 0, timeTick, 0, "Charge"))
+                i = i + 1
 
         else:
             # The system is idle
@@ -92,26 +96,32 @@ with open('output.csv', 'a', newline='') as csvfile:
         ######## Update the value ##########
         # Write the car power information into csv format
         data.append(str(hh) + ':' + str(mm))
+        data2.append(str(hh) + ':' + str(mm))
 
         #print("Charge Num: ", sum(c.charging == "Charge" for c in cars))
 
         for car in cars:
             carNum = sum(c.charging == "Charge" for c in cars)
 
-            A = basic_arrange(carNum)
+            car.valueA = basic_arrange(carNum)
             car.carDuration = timeTick - car.carCreate
-            car.power = powerCorrection(car.carDuration, A)
+            car.power = powerCorrection(car.carDuration, car.valueA)
 
             if car.power == 0:
                 car.charging = "Completed"
+                car.valueA = 0
                 carNum = sum(c.charging == "Charge" for c in cars)
 
-            data.append(str(car.charging))
+            data.append(str(car.valueA))
+            data2.append(str(car.power))
+
         writer.writerow(data)
+        writer.writerow(data2)
 
         # Reset the information write to the csv file of every row
         data = []
-
+        data2 = []
+        j = j + 1
         time = time + time_spacing
         timeTick = timeTick + time_spacing
 
