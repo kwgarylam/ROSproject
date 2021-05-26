25c0ce7efc47b5505b68505a0f63363c3acc32b3
kwgarylam <kwgarylam@gmail.com>
2021-05-26 10:38:59 +0800

    Create KeyPressModule.py

---
+++ reference/KeyPressModule.py
@@ -0,0 +1,29 @@
+#pip3 install pygame
+import pygame.display
+
+def init():
+    pygame.init()
+    win = pygame.display.set_mode((100, 100))
+
+def getKey(keyName):
+    ans = False
+
+    for eve in pygame.event.get():pass
+    keyInput = pygame.key.get_pressed()
+    myKey = getattr(pygame, 'K_{}'.format(keyName))
+    if keyInput[myKey]:
+        ans = True
+    pygame.display.update()
+    
+    return ans
+
+def main():
+    if getKey('a'):
+        print('Key a pressed')
+    if getKey('a'):
+        print('Key a pressed')
+
+if __name__ == '__main__':
+    init()
+    while True:
+        main()
